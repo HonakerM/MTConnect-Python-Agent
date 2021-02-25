49b200d662e91e3063e4a4ca06187880a1efdddb
Michael Honaker <mchonaker@gmail.com>
2021-02-25 11:20:23 -0500

    Update testing and better documentation

--- README.md
+++ README.md
@@ -2,32 +2,49 @@
 ## Purpose
 Python agent for MTConnect applications.
 
-This library helps store, and format data for MTConnect. The 
-MTConnect standard can be found at [mtconnect.org](https://www.mtconnect.org).
-This package does not handle the HTTP aspect of the standard.
-It is designed to be used in conjunction with a wsgi interface like
-flask or django.
+This library helps store, and format data for MTConnect. The MTConnect standard can be found at [mtconnect.org](https://www.mtconnect.org). This package does not handle the HTTP aspect of the standard just the data processing. It is designed to be used in conjunction with a wsgi interface like flask or django.
 
-At the time of writting the library is not feature complete. The current implementation is
-a minimal MTConnect instance operational. This library supports Devices, Components, DataItems, and Steams.
-As well as the probe, current, and sample commands.
+At the time of writting the library is not feature complete. The current implementation is to get a minimal MTConnect instance operational. This library supports Devices, Components, DataItems, and Steams. As well as the probe, current, and sample commands.
 
 ## Use
 To use this library first import the `mtconnect` module.
 
-`import mtconnect`
+```
+import mtconnect
+```
 
 Then create an instance of the MTConnect Agent
 
-`instance = mtconnect.MTConnect()`
+```
+instance = mtconnect.MTConnect()
+```
 
-The instance requires a xml file with the device descriptions. The default location is `./device.xml`. This can
-be changed by setting the loc value as shown below.
 
-`instance = mtconnect.MTconnect(loc='./other_device.xml')`
+The instance requires a xml file with the device descriptions. The default location is `./device.xml`. This can be changed by setting the loc value as shown below. This file is in the format of the `Device` element xml and will be returned by the `probe` command.
+
+```
+instance = mtconnect.MTconnect(loc='./other_device.xml')
+```
+
 
 The instance creation also sets the hostname. This hostname is required by the mtconnect standard in the header of all responses. This can be set by providing the hostname variable as shown below.
 
-`instance = mtconnect.MTconnect(hostname='10.0.0.1:8000')`
+```
+instance = mtconnect.MTconnect(hostname='10.0.0.1:8000')
+```
+
+
+To push data to the buffer use the following command. The `dataId` is the same as the `id` in the `./device.xml` file. Value must be of the right type or it will error.
+```
+instance.push_data(dataId, value)
+```
+
+To access the commands you can run either of the following commands. The corispond to the `probe`, `sample`, `current` endpoints in the mtconnect standard.
+
+```
+instance.probe()
+instance.sample(path=None, start=None, count=None)
+instance.current(at=None, path=None)
+```
 
 
--- tests/storage_test.py
+++ tests/storage_test.py
@@ -12,29 +12,42 @@ import datetime
 
 
 class DataEntity(unittest.TestCase):
+    test_device = None
+    test_item_1 =  None
 
+    def __init__(self, *args, **kwargs):
+        super(DataEntity, self).__init__(*args, **kwargs)
+
+        self.test_device = MTDevice('test_device','1',None)
+        self.test_item_1 = MTDataItem('test_1','test_1','SYSTEM','SAMPLE',self.test_device,self.test_device)
+    
     def testDataCreation(self):
-
-        test_device = MTDevice('test_device','1',None)
-        test_item_1 = MTDataItem('test_1','SYSTEM','SAMPLE',test_device,test_device)
-
-        entity = MTDataEntity(test_item_1,1)
-
+        entity = MTDataEntity(self.test_item_1,1)
 
         self.assertTrue(isinstance(entity.timestamp, type(datetime.datetime.now())))
         self.assertEqual(entity.dataItem.id,'test_1')
         self.assertEqual(entity.value,1)
 
     def testSequenceNumber(self):
-        test_device = MTDevice('test_device','1',None)
-        test_item_1 = MTDataItem('test_1','SYSTEM','SAMPLE',test_device,test_device)
-
-
-        entity = MTDataEntity(test_item_1,1)
+        entity = MTDataEntity(self.test_item_1,1)
         entity.set_sequence(1)
         self.assertEqual(entity.sequence_number,1)
 
 class BufferTest(unittest.TestCase):
+    test_device = None
+    test_item_1 = None
+    test_item_2 = None
+    
+    test_buffer = None
+
+    def __init__(self, *args, **kwargs):
+        super(BufferTest, self).__init__(*args, **kwargs)
+
+        self.test_device = MTDevice('test_device','1',None)
+        self.test_item_1 = MTDataItem('test_1','test_1','SYSTEM','SAMPLE',self.test_device,self.test_device)
+        self.test_item_2 = MTDataItem('test_2','test_2','SYSTEM','SAMPLE',self.test_device,self.test_device)
+        
+        self.test_buffer = MTBuffer(buffer_length=2)
 
     def testBufferCreation(self):
         buffer = MTBuffer()
@@ -47,16 +60,11 @@ class BufferTest(unittest.TestCase):
 
     def testBufferPush(self):
         #initalize buffer
-        buffer = MTBuffer(buffer_length=2)
+        buffer = self.test_buffer
 
-        test_device = MTDevice('test_device','1',None)
-        test_item_1 = MTDataItem('test_1','SYSTEM','SAMPLE',test_device,test_device)
-        test_item_2 = MTDataItem('test_2','SYSTEM','SAMPLE',test_device,test_device)
-
-
-        data_1 = MTDataEntity(test_item_1,1)
-        data_2 = MTDataEntity(test_item_1,2)
-        data_3 = MTDataEntity(test_item_2,1)
+        data_1 = MTDataEntity(self.test_item_1,1)
+        data_2 = MTDataEntity(self.test_item_1,2)
+        data_3 = MTDataEntity(self.test_item_2,1)
 
         buffer.push(data_1)
         self.assertEqual(data_1.sequence_number, 1)
@@ -75,13 +83,11 @@ class BufferTest(unittest.TestCase):
         self.assertEqual(buffer.last_sequence,3)
         self.assertEqual(buffer.first_sequence,2)
         
-        self.assertEqual(buffer.last_value, {'test_1':data_1})
-
     def testBufferGet(self):
-        buffer = MTBuffer(buffer_length=2)
+        buffer = self.test_buffer
 
         test_device = MTDevice('test_device','1','1',None)
-        test_item = MTDataItem('test_1','SYSTEM','SAMPLE',test_device,test_device)
+        test_item = MTDataItem('test_1','test_1','SYSTEM','SAMPLE',test_device,test_device)
         data_1 = MTDataEntity(test_item,1)
         data_2 = MTDataEntity(test_item,2)
         buffer.push(data_1)
