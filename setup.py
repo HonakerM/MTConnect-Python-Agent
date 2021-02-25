465fe8d1a19d8ca4527dd5a58e5341dcdb27508d
Michael Honaker <mchonaker@gmail.com>
2021-02-25 13:47:29 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MTConnect",
-    version="0.2.0",
+    version="0.2.2",
     author="Michael Honaker",
     author_email="mchonaker@gmail.com",
     description="A python agent for MTConnect",
